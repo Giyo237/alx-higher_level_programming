8944170cf022ed93298c9299451e47b88dd87a67
kalifa <kalifkalifa@gmail.com>
2023-11-30 15:57:31 -0800

    read the manual

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-""" match the byte codes 
-	"""
-		from magic_calculation_102 import add, sub
-		if a < b:
+    from magic_calculation_102 import add, sub
+	if a < b:
 		c = add(a, b)
 		for i in range(4,6):
 			c = add(c,i)
 			return (c)
-		else:
+	else:
 			return(sub(a,b))
