5b1084f9f666818cf3b1cabe85a9ae5e7274777b
kalifa <kalifkalifa@gmail.com>
2023-12-04 15:57:00 -0800

    holla

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-if not sentence:
-	sentence = None
-if sentence:
-	sl = len(sentence)
-else:
-	sl = 0
-return (sl, sentence if not sentence else sentence[:1])
+    if not sentence:
+        sentence = None
+    if sentence:
+        sl = len(sentence)
+    else:
+        sl = 0
+    return (sl, sentence if not sentence else sentence[:1])
