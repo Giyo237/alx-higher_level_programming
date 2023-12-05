0138d05ee4bc8614ce107ef0065dcfc091636b54
kalifa <kalifkalifa@gmail.com>
2023-12-04 16:09:40 -0800

    corrected

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 a = 89
 b = 10
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+
+# Swapping the values of a and b
 temp = a
 a = b
 b = temp
+
 print("a={:d} - b={:d}".format(a, b))
