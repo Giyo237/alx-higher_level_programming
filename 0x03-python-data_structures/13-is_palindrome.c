a9e84b706566237929cd94f2476aac18c0b17dd4
kalifa <kalifkalifa@gmail.com>
2023-12-04 17:23:34 -0800

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,96 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * reverse_list - Reverses a linked list
- * @head: Pointer to the head of the list
- *
- * Return: Pointer to the new head of the reversed list
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: head of listint_t
+ * @n: int to add in listint_t list
+ * Return: address of the new element, or NULL if it failed
  */
-listint_t *reverse_list(listint_t *head)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *prev = NULL;
-	listint_t *current = head;
-	listint_t *next = NULL;
+	listint_t *new;
 
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	return prev;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome
- * @head: Pointer to the head of the list
- *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * is_palindrome - identify if a single linked list is palindrome
+ * @head: head of listint_t
+ * Return: 1 if it is palindrome else 0
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow_ptr, *fast_ptr, *prev_slow_ptr;
-	listint_t *second_half, *mid_node;
-	int is_palindrome = 1;
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return 1;
+	if (*head == NULL || head2->next == NULL)
+		return (1);
 
-	slow_ptr = *head;
-	fast_ptr = *head;
-	prev_slow_ptr = *head;
-
-	while (fast_ptr != NULL && fast_ptr->next != NULL)
+	while (head2 != NULL)
 	{
-		fast_ptr = fast_ptr->next->next;
-		prev_slow_ptr = slow_ptr;
-		slow_ptr = slow_ptr->next;
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
 	}
+	aux2 = aux;
 
-	if (fast_ptr != NULL)
+	while (*head != NULL)
 	{
-		mid_node = slow_ptr;
-		slow_ptr = slow_ptr->next;
-	}
-	else
-	{
-		mid_node = NULL;
-	}
-
-	prev_slow_ptr->next = NULL;
-
-	second_half = reverse_list(slow_ptr);
-
-	listint_t *temp1 = *head;
-	listint_t *temp2 = second_half;
-
-	while (temp1 != NULL && temp2 != NULL)
-	{
-		if (temp1->n != temp2->n)
+		if ((*head)->n != aux2->n)
 		{
-			is_palindrome = 0;
-			break;
+			free_listint(aux);
+			return (0);
 		}
-
-		temp1 = temp1->next;
-		temp2 = temp2->next;
+		*head = (*head)->next;
+		aux2 = aux2->next;
 	}
-
-	second_half = reverse_list(second_half);
-
-	if (mid_node != NULL)
-	{
-		prev_slow_ptr->next = mid_node;
-		mid_node->next = second_half;
-	}
-	else
-	{
-		prev_slow_ptr->next = second_half;
-	}
-
-	return is_palindrome;
+	free_listint(aux);
+	return (1);
 }
