247a45e3f8ba50a0b6608e7d65d6bbc133cc37b6
kalifa <kalifkalifa@gmail.com>
2024-03-13 02:28:43 -0700

    incr

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12
+  value: 12,
+  incr() {
+	  this.value++;
+  }
 };
 console.log(myObject);
 /*
 YOUR CODE HERE
 */
-myObject.incr = function () {
-	this.value++;
-};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
